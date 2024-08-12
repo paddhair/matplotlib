7392e874317d8e8b68352993fd2ccab03e09e047
paddhair <paddhair@gmail.com>
2024-08-12 23:11:48 +0330

    new_shayan

---
+++ new.py
@@ -0,0 +1,9 @@
+# shayan dolat yari
+import numpy as np
+import matplotlib.pyplot as plt 
+x=np.linspace(1,100,1000)
+y=x**2
+plt.plot(x,y)
+plt.grid()
+plt.show()
+'''________________________________________'''
